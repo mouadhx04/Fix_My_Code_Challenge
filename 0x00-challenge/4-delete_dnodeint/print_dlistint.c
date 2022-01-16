7ed7670fc182fd1c8fd415a44cdb5fbeecc19150
root <root@Mouadh.localdomain>
2022-01-16 23:43:24 +0100

    1

--- 0x00-challenge/4-delete_dnodeint/print_dlistint.c
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints a doubly linkedlist of integers
+ * print_dlistint - Print a doubly linkedlist of integers
  *
  * @h: A pointer to the first element of a list
  *
